2c93c96ad180283f037e0049455daca628dd21ca
Ashok Poudel <ashok.poudel@gmail.com>
2023-08-29 11:34:07 +0545

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
+set -e # Exit on any error
 
 # Update package lists
 sudo apt-get update
 
 # Install required packages
-sudo apt-get install -y ca-certificates curl gnupg
-
-# Create directory for keyrings
-sudo install -m 0755 -d /etc/apt/keyrings
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
 
 # Import Docker GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
-
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # Add Docker repository to sources.list.d
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Update package lists again
 sudo apt-get update
 
 # Install Docker packages
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Optionally install plugins (if they are available in your distribution)
+# sudo apt-get install -y docker-buildx-plugin docker-compose-plugin
