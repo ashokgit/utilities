e6ff2c5781a3ee032577022a029053229cb74d80
Ashok Poudel <ashok.poudel@gmail.com>
2023-08-29 11:20:44 +0545

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -9,11 +9,13 @@ sudo apt-get install -y ca-certificates curl gnupg
 # Create directory for keyrings
 sudo install -m 0755 -d /etc/apt/keyrings
 
-# Add Docker repository to sources.list.d
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 # Import Docker GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
+
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add Docker repository to sources.list.d
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Update package lists again
 sudo apt-get update
